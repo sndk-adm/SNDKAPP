237cc9b059d6c9092327bb991cf40ff184bda88c
sndk-adm <kajiwara@ndk.co.th>
2021-10-21 16:08:06 +0700

    phptest_20211021_0408

--- phptest.php
+++ phptest.php
@@ -11,7 +11,7 @@ function convert_enc($str){
     return mb_convert_encoding($str, $to_enc, $from_enc);
 }
 
-$dsn = 'pgsql:dbname=postgresql-angular-08182 host=ec2-3-211-245-154.compute-1.amazonaws.com port=5432';
+$dsn = 'pgsql:dbname=postgresql-angular-08182 host=ec2-3-211-245-154.compute-1.amazonaws.com  port=5432';
 $user = 'hhmxfllafjsciw';
 $password = 'dd16a7e2edfc599031962ac809ded8807cfe6d41cbb2c8b73681578bf5841f5f';
 
