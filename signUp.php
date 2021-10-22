4310e14004a7db716676b7925fa1b7758bab5840
sndk-adm <kajiwara@ndk.co.th>
2021-10-22 12:10:03 +0700

    post_20211022_1209

--- signUp.php
+++ signUp.php
@@ -5,29 +5,27 @@
 <?php
 require_once('config.php');//config.phpの読み込み
 
-print('接続に成功しました。<br>');
-
-// //データベースへ接続、テーブルがない場合は作成
-// try {
-//   $pdo = new PDO(DSN, DB_USER, DB_PASS);
+//データベースへ接続、テーブルがない場合は作成
+try {
+  $pdo = new PDO(DSN, DB_USER, DB_PASS);
   
-//   // PDO::ATTR_ERRMODE属性でPDO::ERRMODE_EXCEPTIONの値を設定することでエラーが発生したときに、//
-//   // PDOExceptionの例外（エラー）を投げる。説明 https://w.atwiki.jp/nicepaper/pages/151.html//
-//   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // PDO::ATTR_ERRMODE属性でPDO::ERRMODE_EXCEPTIONの値を設定することでエラーが発生したときに、//
+  // PDOExceptionの例外（エラー）を投げる。説明 https://w.atwiki.jp/nicepaper/pages/151.html//
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
-//   //例外処理 作成済みのテーブルを作ろうとするエラーを防ぐ。
-//   $pdo->exec("create table if not exists login_emp(
-//       id serial primary key,
-//       empcode char(10) unique,
-//       prefix_en int not null,
-//       name_en varchar(20) not null,
-//       surname_en varchar(20) not null,
-//       date_birth date not null,
-//       email char(30) unique,
-//       password varchar(10) not null)");
-//   }
-// catch (Exception $e) {
-//   echo $e->getMessage() . PHP_EOL;}
+  //例外処理 作成済みのテーブルを作ろうとするエラーを防ぐ。
+  $pdo->exec("create table if not exists login_emp(
+      id serial primary key,
+      empcode char(10) unique,
+      prefix_en int not null,
+      name_en varchar(20) not null,
+      surname_en varchar(20) not null,
+      date_birth date not null,
+      email char(30) unique,
+      password varchar(10) not null)");
+  }
+catch (Exception $e) {
+  echo $e->getMessage() . PHP_EOL;}
 
 $empcode = ($_POST['EMPCODE']);
 $prefix_en = ($_POST['PREFIX_EN']);
