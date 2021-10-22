c4b953dfb5da91f319ac3735e77db4176f36e740
sndk-adm <kajiwara@ndk.co.th>
2021-10-22 10:40:44 +0700

    post_20211022_1040

--- phptest.php
+++ phptest.php
@@ -15,6 +15,18 @@ $dsn = 'pgsql:dbname=dfl9gst6l1jfl3 host=ec2-3-211-245-154.compute-1.amazonaws.c
 $user = 'hhmxfllafjsciw';
 $password = 'dd16a7e2edfc599031962ac809ded8807cfe6d41cbb2c8b73681578bf5841f5f';
 
+$empcode = ($_POST['EMPCODE']);
+$prefix_en = ($_POST['PREFIX_EN']);
+$name_en = ($_POST['NAME']);
+$surname_en = ($_POST['SURNAME']);
+$date_birth = ($_POST['DATE_BIRTH']);
+
+echo "$empcode" "\n"
+echo "$prefix_en" "\n"
+echo "$name_en"  "\n"
+echo "$surname_en" "\n"
+echo "$date_birth" "\n"
+
 try{
     $dbh = new PDO($dsn, $user, $password);
 
