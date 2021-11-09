47cdcd0a407862d0cacf40704cfe3dfa44ae39f8
sndk-adm <kajiwara@ndk.co.th>
2021-11-09 15:29:29 +0700

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '08bcef4be6d91bb9f15576ff15a178a0277f1aac',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '08bcef4be6d91bb9f15576ff15a178a0277f1aac',
+            'dev_requirement' => false,
+        ),
+    ),
+);
