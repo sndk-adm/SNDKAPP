93faabb99f8386aee5799595b88145fd9dd3a2c4
sndk-adm <kajiwara@ndk.co.th>
2021-11-11 16:55:49 +0700

    session

--- login.php
+++ login.php
@@ -16,54 +16,55 @@
     // // セッションの有効期限を5分に設定
     // // session_set_cookie_params(60 * 5);
    
-    // //データベース接続情報
-    //   $dsn = 'pgsql:dbname=dfl9gst6l1jfl3 host=ec2-3-211-245-154.compute-1.amazonaws.com  port=5432';
-    //   $user = 'hhmxfllafjsciw';
-    //   $password = 'dd16a7e2edfc599031962ac809ded8807cfe6d41cbb2c8b73681578bf5841f5f';
-
+    //データベース接続情報
+      $dsn = 'pgsql:dbname=dfl9gst6l1jfl3 host=ec2-3-211-245-154.compute-1.amazonaws.com  port=5432';
+      $user = 'hhmxfllafjsciw';
+      $password = 'dd16a7e2edfc599031962ac809ded8807cfe6d41cbb2c8b73681578bf5841f5f';
              
-    //   //emailデータ型の検証 
-    //   if (filter_var($email,FILTER_VALIDATE_EMAIL) === false) {
-    //   echo $email;
-    //   echo '<br>';
-    //   echo 'メールアドレスが不正です。';
-    //   return false;  }
+      //emailデータ型の検証 
+      if (filter_var($email,FILTER_VALIDATE_EMAIL) === false) {
+      echo $email;
+      echo '<br>';
+      echo 'メールアドレスが不正です。';
+      return false;  }
 
-    //   //DB内でPOSTされたメールアドレスを検索
-    //   try {
-    //   $pdo = new PDO($dsn, $user, $password);
-    //   $stmt = $pdo->prepare('select * from login_emp where email = ?');
-    //   $stmt->execute([$email]);
-    //     //取得したでレコードを1行返す
-    //     $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    //   } 
-    //   catch (\Exception $e) {
-    //   echo 'メールアドレス又はパスワードが間違っています。';
-    //   echo '<br>';
-    //   echo $e->getMessage() . PHP_EOL;
-    //   }
+      //DB内でPOSTされたメールアドレスを検索
+      try {
+      $pdo = new PDO($dsn, $user, $password);
+      $stmt = $pdo->prepare('select * from login_emp where email = ?');
+      $stmt->execute([$email]);
+        //取得したでレコードを1行返す
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      } 
+      catch (\Exception $e) {
+      echo 'メールアドレス又はパスワードが間違っています。';
+      echo '<br>';
+      echo $e->getMessage() . PHP_EOL;
+      }
 
-    //   //emailがDB内に存在しているかissetで確認
-    //   if (isset($row['email']) === false) {
-    //     echo '<br>';
-    //     echo '従業員コードが見当たりません';
-    //   return false;
-    //   }
+      //emailがDB内に存在しているかissetで確認
+      if (isset($row['email']) === false) {
+        echo '<br>';
+        echo '従業員コードが見当たりません';
+      return false;
+      }
 
-    //   //パスワード確認
-    //   if ("$login_pass" == $row['login_pass']) {
-    //     // $_session['name_en']=$row['name_en'];
-    //     // $_session['email']=$row['email'];
-    //     // $_session['login_pass']=$row['login_pass'];
-     
+      //パスワード確認
+      if ("$login_pass" == $row['login_pass']) {
+        $_session['name_en_db']=$row['name_en'];
+        $_session['email_db']=$row['email'];
+        $_session['login_pass_db']=$row['login_pass'];
+        echo $_session['name_en_db'];
+        echo '<br>';
+        echo $_session['email_db'];
+        echo '<br>';
+        echo $_session['login_pass_db'];
     //   // header('Location: https://sndk-adm.herokuapp.com/home.php');
-    //   // 
-    // } 
-      
-    //   else {
-    //   echo '<br>';
-    //   echo 'パスワードが間違っています。';
-    //   return false;
-    //   }
+    } 
+      else {
+      echo '<br>';
+      echo 'パスワードが間違っています。';
+      return false;
+      }
 
     ?>
