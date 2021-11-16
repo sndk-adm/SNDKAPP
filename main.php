e89d2f87034eb1a23786735551bac5162b352bff
sndk-adm <kajiwara@ndk.co.th>
2021-11-16 14:24:31 +0700

    CSS

--- main.php
+++ main.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>MAIN MENU</title>
-    <link rel="stylesheet" href="bootstrap/css" type="text/css" charset="utf-8">
-    <link rel="stylesheet" href="bookmin.css" type="text/css" charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" charset="utf-8">
+    <link rel="stylesheet" href="css/bookmin.css" type="text/css" charset="utf-8">
+
 </head>
 <body>
 <!-- PageHeader -->
